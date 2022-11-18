1e6a2f4439e50c706e0f951e255237d1cc4512be
UserVasyk <106909139+UserVasyk@users.noreply.github.com>
2022-11-18 18:00:01 +0200

    update

---
+++ src/fetchCountries.js
@@ -0,0 +1,12 @@
+function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.png,languages`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    console.log(response);
+    return response.json();
+  });
+}
+export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 
+import { fetchCountries } from './fetchCountries';
+const refs = {
+  inputCountry: document.querySelector('#search-box'),
+};
 const DEBOUNCE_DELAY = 300;
+refs.inputCountry.addEventListener(
+  'input',
+  debounce(onInputCountry, DEBOUNCE_DELAY)
+);
+
+function onInputCountry(evt) {
+  if (evt.target.value === '') {
+    return;
+  }
+  const name = evt.target.value;
+  fetchCountries(name.trim())
+    .then(responce => {
+      console.log(responce);
+      console.log(responce);
+    })
+    .catch(error => console.log('error', error));
+}
