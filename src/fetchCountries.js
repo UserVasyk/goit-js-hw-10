ead0ba7c164c81bcc1440b80b165c61bcb9a5839
UserVasyk <106909139+UserVasyk@users.noreply.github.com>
2022-11-18 19:52:24 +0200

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,14 @@
 function fetchCountries(name) {
   return fetch(
     `https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.png,languages`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    console.log(response);
-    return response.json();
-  });
+  )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      console.log(response);
+      return response.json();
+    })
+    .then(r => r.articles);
 }
 export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 
 import { fetchCountries } from './fetchCountries';
+import countryTemplate from './templates/countryTemplate.hbs';
 const refs = {
   inputCountry: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
 };
 const DEBOUNCE_DELAY = 300;
 refs.inputCountry.addEventListener(
@@ -19,7 +21,10 @@ function onInputCountry(evt) {
   fetchCountries(name.trim())
     .then(responce => {
       console.log(responce);
-      console.log(responce);
+      refs.countryList.insertAdjacentHTML(
+        'beforeend',
+        countryTemplate(responce)
+      );
     })
     .catch(error => console.log('error', error));
 }
